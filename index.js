ab4a7bd56db9c2f38808811a202e218409c5fe42
Molly <dudumikah@gmail.com>
2023-12-27 18:48:51 +0500

    Second Commit

--- index.js
+++ index.js
@@ -18,3 +18,40 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 900)
     });
 });
+
+
+// for hover aimation change bg location
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const firstHoverButton = document.querySelector('.hover-button__first')
+    const secondHoverButton = document.querySelector('.hover-button__second')
+    const bg = document.querySelector('body');
+
+    firstHoverButton.addEventListener('mouseover', function() {
+        bg.style.backgroundPosition = "25% 50%";
+    });
+
+    secondHoverButton.addEventListener('mouseover', function() {
+        bg.style.backgroundPosition = "75% 50%";
+    });
+
+    firstHoverButton.addEventListener('mouseout', function() {
+        bg.style.backgroundPosition = "50% 50%";
+    });
+
+    secondHoverButton.addEventListener('mouseout', function() {
+        bg.style.backgroundPosition = "50% 50%";
+    });
+});
+
+
+// for pop-up
+
+document.getElementById('openPopup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
+
